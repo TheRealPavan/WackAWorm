5b687e0cc93a3b89411da3a3a476ee04196a9548
Pavan <bodapatip@s.dcsdk12.org>
2021-02-17 11:27:10 -0700

    added level1

--- app/src/main/java/org/legendtitans/wackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/wackaworm/MainActivity.java
@@ -2,13 +2,30 @@ package org.legendtitans.wackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @SuppressLint("ClickableViewAccessibility")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
 }
+
