cf24b22288ad7474db0abec40144c06b3aeb133f
Pavan <bodapatip@s.dcsdk12.org>
2021-02-18 12:16:43 -0700

    added level1

--- app/src/main/java/org/legendtitans/wackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/wackaworm/MainActivity.java
@@ -3,11 +3,13 @@ package org.legendtitans.wackaworm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 // TODO Auto-generated method stub
-                Log.i("imageviewandontouchlistener", "playImageView onTouch");
-                return false;
+                Toast.makeText(getApplicationContext(), "I was touched, oh god call the police", Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
     }
